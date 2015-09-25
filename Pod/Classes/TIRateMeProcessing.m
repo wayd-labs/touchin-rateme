86b6f91ce0bca561520edc802ef350f28f78e36a
bobile-touchin <aleksey.alekseenkov@touchin.ru>
2015-09-25 18:59:06 +0300

    add analytics events

--- Pod/Classes/TIRateMeProcessing.m
+++ Pod/Classes/TIRateMeProcessing.m
@@ -8,6 +8,7 @@
 
 #import "TIRateMeProcessing.h"
 #import "TITrivia.h"
+#import "TIAnalytics.h"
 
 @interface TIRateMeProcessing()
 
@@ -111,13 +112,16 @@ NSString* UD_STAGE_KEY = @"TIRateMeStage";
 
 - (void) yesButtonTap {
     if ([self getStage] == TIRateMeStageLike) {
+        [TIAnalytics.shared trackEvent:@"RATEME-CELL_LIKE_ANSWERED" properties:@{@"answer": @"YES"}];
         [self setStage:TIRateMeStageAppStore];
     }
     else if ([self getStage] == TIRateMeStageAppStore) {
+        [TIAnalytics.shared trackEvent:@"RATEME-CELL_APPSTORE_ANSWERED" properties:@{@"answer": @"YES"}];
         [self sendToAppstore];
         [self finish:TRUE];
     }
     else if ([self getStage] == TIRateMeStageFeedback) {
+        [TIAnalytics.shared trackEvent:@"RATEME-CELL_FEEDBACK_ANSWERED" properties:@{@"answer": @"YES"}];
         [self askFeedback];
         [self finish:TRUE];
     }
@@ -125,9 +129,16 @@ NSString* UD_STAGE_KEY = @"TIRateMeStage";
 
 - (void) noButtonTap {
     if ([self getStage] == TIRateMeStageLike) {
+        [TIAnalytics.shared trackEvent:@"RATEME-CELL_FEEDBACK_ANSWERED" properties:@{@"answer": @"NO"}];
         [self setStage:TIRateMeStageFeedback];
     }
     else {
+        if ([self getStage] == TIRateMeStageAppStore) {
+            [TIAnalytics.shared trackEvent:@"RATEME-CELL_APPSTORE_ANSWERED" properties:@{@"answer": @"NO"}];
+        }
+        else if ([self getStage] == TIRateMeStageFeedback) {
+            [TIAnalytics.shared trackEvent:@"RATEME-CELL_FEEDBACK_ANSWERED" properties:@{@"answer": @"NO"}];
+        }
         [self finish:FALSE];
     }
 }
